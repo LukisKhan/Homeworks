2943dddbb616bd8afef0a1b5aa5c8b2c3564793c
Luat Pham <luat32@gmail.com>
2019-07-16 19:43:40 -0500

    Added MCV association practice

---
+++ W8D2/association/app/models/enrollment.rb
@@ -0,0 +1,15 @@
+class Enrollment < ApplicationRecord
+
+    belongs_to(:students, {
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+    })
+
+    belongs_to(:courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+    )
+
+end
