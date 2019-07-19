cc756d8d173379679d519fd965aba1aeaa27769f
Luat Pham <luat32@gmail.com>
2019-07-19 13:32:03 -0500

    Added has_many and belongs_to classes

--- W8D5/Active_record_lite/lib/03_associatable.rb
+++ W8D5/Active_record_lite/lib/03_associatable.rb
@@ -10,23 +10,50 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    
   end
 
   def table_name
-    # ...
+    
   end
 end
 
+# options = BelongsToOptions.new(:owner)
+# options.foreign_key # => :owner_id
+# options.primary_key # => :id
+# options.class_name # => "Owner"
+
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    
+    default = {
+      primary_key: :"id",
+      foreign_key: :"#{name}_id",
+      class_name: "#{name.capitalize}"
+    }
+
+    options = options.empty? ? default : options
+    
+    self.primary_key = options[:primary_key]
+    self.foreign_key = options[:foreign_key]
+    self.class_name  = options[:class_name]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    
+    default = {
+      primary_key: :"id",
+      foreign_key: :"#{self_class_name.downcase}_id",
+      class_name: "#{name.singularize.capitalize}"
+    }
+
+    options = options.empty? ? default : options
+    
+    self.primary_key = options[:primary_key]
+    self.foreign_key = options[:foreign_key]
+    self.class_name  = options[:class_name]
   end
 end
 
