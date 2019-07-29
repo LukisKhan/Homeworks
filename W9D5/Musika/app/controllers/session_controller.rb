79e10ef4eef048a424f28549becbb0fd730796a2
Luat Pham <luat32@gmail.com>
2019-07-29 07:49:03 -0700

    Music app created

---
+++ W9D5/Musika/app/controllers/session_controller.rb
@@ -0,0 +1,28 @@
+class SessionController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:email]
+    )
+    if user.nil?
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    elsif !user.activated
+      flash.now[:errors] = ["Activate account first"]
+      render :new
+    else
+      login_user!(user)
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to new_session_url
+  end
+end
--- W9D5/Musika/config/routes.rb
+++ W9D5/Musika/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get 'users/new'
-  # get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new]
+  resource :session, only: [:new, :create, :destroy]
 end
