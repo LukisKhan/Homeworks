e9657a20e478e06b5f255b8dafde79525463e9ee
Luat Pham <luat32@gmail.com>
2019-08-07 10:43:03 -0500

    Finished elephan constructor function

---
+++ W11D3/javascript.js
@@ -0,0 +1,31 @@
+function titleize( names, printOut ) {
+    let title = names.map(name => `Mr. ${name} Jingleheimer Schmidt`);
+    printOut(title);
+}
+
+titleize( ["Mary", "Brian", "Leo"], (names) => {
+    names.forEach(name => console.log(name));
+});
+
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} roars 'phrRRRRRRRRRRR!!!!!!!`)
+};
+
+Elephant.prototype.grow = function () {
+    this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (newTrick) {
+    this.tricks.push(newTrick);
+};
+
+Elephant.prototype.play = function () {
+    randomTrickIndex = Math.floor(Math.random() * this.tricks.length);
+    console.log(`${this.name} is ${this.tricks[randomTrickIndex()]}!`);
+};
