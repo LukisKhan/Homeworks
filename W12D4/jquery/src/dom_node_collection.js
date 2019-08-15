2efebfc220bf06fd6f01429bd1cc9c126a5b9d74
Luat Pham <luat32@gmail.com>
2019-08-15 15:27:05 -0500

    Added each, append, attr, removeClass, and AddClass methods

--- W12D4/jquery/src/dom_node_collection.js
+++ W12D4/jquery/src/dom_node_collection.js
@@ -7,17 +7,55 @@ class DOMNodeCollection {
   //return a DOMNodeCollection instance
   html(argHTML) {
     if (argHTML === undefined) return this.elementArray[0].innerHTML;
-    this.each(argHTML);
+    this.each(ele => {
+      ele.innerHTML = argHTML;
+    });
   }
   empty () {
-    this.each('');
+    this.each( ele => {
+      ele.innerHTML = ""; 
+    });
   }
-  each (argChange) {
+  // each without callback
+  // each (argChange) {
+  //   for (let i = 0; i < this.elementArray.length; i++) {
+  //     this.elementArray[i].innerHTML = argChange;
+  //   }
+  // }
+
+  //each with callback
+  each(callback) {
+    this.elementArray.forEach(callback);
+  }
+
+  append(childrenTags) {
     for (let i = 0; i < this.elementArray.length; i++) {
-      this.elementArray[i].innerHTML = argChange;
+      this.elementArray[i].innerHTML += childrenTags;
     }
   }
 
+  attr (key, value) {
+    if (value === undefined) return this.elementArray[0].getAttribute(key);
+    this.each(ele => ele.setAttribute(key, value));
+  }
+
+  addClass (value) {
+    this.each( ele => {
+      let prevAttribute = ele.getAttribute("class");
+      if (prevAttribute === undefined) {
+        ele.setAttribute("class", value);
+      } else {
+        const newAttri = prevAttribute.concat(" ").concat(value);
+        ele.setAttribute("class", newAttri);
+      }
+    });
+  }
+
+  removeClass (currentClass) {
+    this.each( ele => {
+      ele.classList.remove(currentClass);
+    });
+  }
 }
 
 module.exports = DOMNodeCollection;
