e019070fda7478b9184d2042e0440822b011b5e1
Luat Pham <luat32@gmail.com>
2019-08-27 18:29:07 -0500

    Added item detail container

--- W14D1/pokedex/frontend/components/items/item_detail_container.js
+++ W14D1/pokedex/frontend/components/items/item_detail_container.js
@@ -3,7 +3,7 @@ import ItemDetail from './item_detail';
 import { selectPokemonItem } from '../../reducers/selectors';
 
 const mapStateToProps = (state, { match }) => ({
-  item: ""
+  item: selectPokemonItem(state, parseInt(match.params.itemId))
 });
 
 export default connect(mapStateToProps)(ItemDetail);
