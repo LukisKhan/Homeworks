2fc2e173f438daee2edc631e65347f6a9504aff7
Luat Pham <luat32@gmail.com>
2019-10-24 16:15:46 -0500

    Finished selection sort

--- W22D4/selection_sort_project/lib/selection_sort.js
+++ W22D4/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,18 @@
 function swap(arr, index1, index2) {
-
+    return [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
 }
 
 function selectionSort(arr) {
-
+    for(let i = 0; i < arr.length ; i++) {
+        minIdx = i;
+        for(let j = minIdx; j < arr.length; j++) {
+            if(arr[minIdx] > arr[j]) {
+                minIdx = j;
+            }
+        }
+        swap(arr, minIdx, i)
+    }
+    return arr
 }
 
 module.exports = {
