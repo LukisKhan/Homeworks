c88a519d31e4d2713a4f1a7685898f5962fe7ed5
Luat Pham <luat32@gmail.com>
2019-10-24 16:48:11 -0500

    Changed indexes to have names

--- W22D4/insertion_sort_project/lib/insertion_sort.js
+++ W22D4/insertion_sort_project/lib/insertion_sort.js
@@ -1,12 +1,13 @@
 function insertionSort(arr) {
     for(let i = 1; i < arr.length; i++) {
-        let currentEle = arr[i];
-        for(var j = i - 1; j >= 0 && currentEle < arr[j]; j--) {
-            arr[j + 1] = arr[j]
+        let unSortedEle = arr[i]
+        let sortedIdx = i - 1;
+        for (sortedIdx; sortedIdx >= 0 && unSortedEle < arr[sortedIdx]; sortedIdx-- ) {
+            let swapIdx = sortedIdx + 1;
+            arr[swapIdx] = arr[sortedIdx]
         }
-        arr[j + 1] = currentEle;
+        arr[sortedIdx+1] = unSortedEle;
     }
-    return arr;
 }
 
 module.exports = {
