0d71e8f8810f56028380911bcd35bc79e9f83b20
Luat Pham <luat32@gmail.com>
2019-10-25 14:38:32 -0500

    Started sorts project

--- W22D4/insertion_sort_project/lib/insertion_sort.js
+++ W22D4/insertion_sort_project/lib/insertion_sort.js
@@ -1,12 +1,21 @@
 function insertionSort(arr) {
-    for(let i = 1; i < arr.length; i++) {
-        let unSortedEle = arr[i]
-        let sortedIdx = i - 1;
-        for (sortedIdx; sortedIdx >= 0 && unSortedEle < arr[sortedIdx]; sortedIdx-- ) {
-            let swapIdx = sortedIdx + 1;
-            arr[swapIdx] = arr[sortedIdx]
+    // for(let i = 1; i < arr.length; i++) {
+    //     let unSortedEle = arr[i]
+    //     let sortedIdx = i - 1;
+    //     for (sortedIdx; sortedIdx >= 0 && unSortedEle < arr[sortedIdx]; sortedIdx-- ) {
+    //         let swapIdx = sortedIdx + 1;
+    //         arr[swapIdx] = arr[sortedIdx]
+    //     }
+    //     arr[sortedIdx+1] = unSortedEle;
+    // }
+
+    for (let i = 1; i < arr.length; i++) {
+        let curEle = arr[i]
+        let swapIdx = i - 1;
+        while(swapIdx >= 0 && curEle < arr[swapIdx] ) {
+            arr[swapIdx+1] = arr[swapIdx--]
         }
-        arr[sortedIdx+1] = unSortedEle;
+        arr[swapIdx+1] = curEle;
     }
 }
 
