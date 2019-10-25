9fa3e350ebc414f07132db2c494811be4708f52f
Luat Pham <luat32@gmail.com>
2019-10-25 15:09:43 -0500

    Finished sorting project

--- W22D5/merge_sort_project/lib/merge_sort.js
+++ W22D5/merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,34 @@
 function merge(array1, array2) {
+    let merged = [];
 
+    while (array1.length || array2.length) {
+        let ele1 = array1.length ? array1[0] : Infinity;
+        let ele2 = array2.length ? array2[0] : Infinity;
+
+        let next;
+        if (ele1 < ele2) {
+            next = array1.shift();
+        } else {
+            next = array2.shift();
+        }
+
+        merged.push(next);
+    }
+
+    return merged;
 }
 
 function mergeSort(array) {
+    if (array.length < 2) {
+        return array
+    }
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid);
 
+    let sortLeft = mergeSort(left);
+    let sortRight = mergeSort(right);
+    return merge(sortLeft, sortRight);
 }
 
 module.exports = {
--- W22D5/radix_sort_project/lib/radix_sort.js
+++ W22D5/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,42 @@
 function radixSort(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    let negatives = arr.filter(el => el < 0);
+    let negSorted = [];
 
+    if (negatives.length > 0) {
+        negSorted = radixSort(negatives.map(el => Math.abs(el)))
+        .reverse()
+        .map(el => -el)
+    }
+    let pos = arr.filter(el => el >= 0);
+    let maxDigits = getMaxDigits(pos);
+
+    for (let k = 0; k < maxDigits; k++) {
+        let buckets = Array.from({length: 10}, () => []);
+        // let buckets = Array(10).fill([]);
+        
+        for (let i = 0; i < pos.length; i++) {
+            let digit = getDigitFrom(pos[i], k);
+            buckets[digit].push (pos[i]);
+        }
+        pos = [].concat(...buckets);
+
+    }
+    return negSorted.concat(pos);
+}
+
+const getDigitFrom = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+
+function getMaxDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+    }
+    return maxDigits;
 }
 
 module.exports = {
