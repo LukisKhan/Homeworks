a0e1cf5eb3ac33729fd65ec24eb001a0e991f94c
Luat Pham <luat32@gmail.com>
2019-10-31 16:02:21 -0500

    Finished binary search tree project

--- W23D4/bst_project/lib/leet_code_108.js
+++ W23D4/bst_project/lib/leet_code_108.js
@@ -3,5 +3,12 @@
 
 
 function sortedArrayToBST(nums) {
-
+  if(!nums.length) return null;
+  let midIdx = Math.floor(nums.length/2);
+  let rootNode = new TreeNode(nums[midIdx]);
+  let leftSortedArr = nums.slice(0, midIdx);
+  let rightSortedArr = nums.slice(midIdx + 1);
+  rootNode.left = sortedArrayToBST(leftSortedArr);
+  rootNode.right = sortedArrayToBST(rightSortedArr);
+  return rootNode;
 }
