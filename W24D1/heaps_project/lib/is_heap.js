3c633c28a47f6906cd34e7015ec5986b9d98f729
Luat Pham <luat32@gmail.com>
2019-11-04 15:49:54 -0600

    Finished Heaps Project

--- W24D1/heaps_project/lib/is_heap.js
+++ W24D1/heaps_project/lib/is_heap.js
@@ -1,6 +1,52 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+    if (array[idx] === undefined) return true;
+    let leftIdx = idx * 2;
+    let rightIdx = idx * 2 + 1;
+    let leftVal = array[leftIdx] === undefined ? -Infinity : array[leftIdx];
+    let rightVal = array[rightIdx] === undefined ? -Infinity : array[rightIdx];
+    console.log("array", array)
+    console.log("idx", idx)
+    console.log("current", array[idx])
+    console.log("leftChild", leftVal)
+    console.log("rightChild", rightVal)
+    return array[idx] > leftVal && array[idx] > rightVal
+        && isMaxHeap(array, leftIdx)
+        && isMaxHeap(array, rightIdx);
 
+
+
+    // console.log(array)
+    // if (siftDown(array, idx) === false) return false;
+    // return true;
+}
+
+function getParent(idx) {
+    return Math.floor(idx / 2);
+}
+function getLeftChild(idx){
+    return idx * 2
+}
+function getRightChild(idx) {
+    return idx * 2 + 1
+}
+function siftDown(array, idx){
+
+    let leftIdx = getLeftChild(idx);
+    let rightIdx = getRightChild(idx);
+    let myArr = array;
+    let leftChild = myArr[leftIdx] || -Infinity;
+    let rightChild = myArr[rightIdx] || -Infinity;
+    let currVal = myArr[idx];
+    console.log("idx",idx)
+    console.log("current",currVal)
+    console.log("leftChild",leftChild)
+    console.log("rightChild",rightChild)
+    if (currVal) {
+        return currVal > leftChild && currVal > rightChild
+            && siftDown(array, leftIdx)
+            && siftDown(array, rightIdx)  
+    }
 }
 
 
--- W24D1/heaps_project/lib/max_heap.js
+++ W24D1/heaps_project/lib/max_heap.js
@@ -5,7 +5,50 @@ class MaxHeap {
     getParent(idx) {
         return Math.floor(idx / 2);
     }
-    
+    getLeftChild(idx){
+        return idx * 2
+    }
+    getRightChild(idx) {
+        return idx * 2 + 1
+    }
+    insert(val) {
+        this.array.push(val)
+        this.siftUp(this.array.length - 1)
+    }
+    siftUp(idx){
+        if(idx === 1) return;
+        let parentIdx = this.getParent(idx);
+        if(this.array[idx] > this.array[parentIdx]){
+            [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]];
+            this.siftUp(parentIdx);
+        }
+    }
+    deleteMax(){
+        if(this.array.length === 1) return null
+        if(this.array.length === 2) return this.array.pop();
+        let max = this.array[1];
+        this.array[1] = this.array.pop();
+        this.siftDown(1);
+        return max;
+    }
+    siftDown(idx){
+
+        let leftIdx = this.getLeftChild(idx);
+        let rightIdx = this.getRightChild(idx);
+        let myArr = this.array;
+        let leftChild = myArr[leftIdx] || -Infinity;
+        let rightChild = myArr[rightIdx] || -Infinity;
+        let currVal = myArr[idx];
+        if(currVal > leftChild && currVal > rightChild) return;
+        if(leftChild > rightChild) {
+            [myArr[idx], myArr[leftIdx]] = [myArr[leftIdx], myArr[idx]]
+            this.siftDown(leftIdx);
+        } else {
+            [myArr[idx], myArr[rightIdx]] = [myArr[rightIdx], myArr[idx]]
+            this.siftDown(rightIdx);
+        }
+    }
+
 }
 
 module.exports = {
