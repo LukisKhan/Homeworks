cf881a35aebdb7fb728fd3783345833214be3ea6
Luat Pham <luat32@gmail.com>
2019-11-04 12:09:54 -0600

    Adding heaps project

---
+++ W24D1/heaps_project/lib/max_heap.js
@@ -0,0 +1,13 @@
+class MaxHeap {
+    constructor(){
+        this.array = [null];
+    }
+    getParent(idx) {
+        return Math.floor(idx / 2);
+    }
+    
+}
+
+module.exports = {
+    MaxHeap
+};
